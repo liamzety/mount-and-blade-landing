31b32c52d8e8c9b1898b7fa155dc19876635ddac
liamzety <58632095+liamzety@users.noreply.github.com>
2020-12-03 11:34:39 +0200

    Initial dist subtree commit

---
+++ dist/precache-manifest.db7f4867ce316164b250ff2944353ee2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60e37c6f7b2c086a4fd7",
+    "url": "/mount-and-blade-landing/css/app.85784a95.css"
+  },
+  {
+    "revision": "a57342759eec35562e38c5ed1dc57bf2",
+    "url": "/mount-and-blade-landing/index.html"
+  },
+  {
+    "revision": "7cd9da36feed31ee193f",
+    "url": "/mount-and-blade-landing/js/about.e372ffa2.js"
+  },
+  {
+    "revision": "60e37c6f7b2c086a4fd7",
+    "url": "/mount-and-blade-landing/js/app.11a49a80.js"
+  },
+  {
+    "revision": "3ef5f46c446fc20bbff2",
+    "url": "/mount-and-blade-landing/js/chunk-vendors.3aa45973.js"
+  },
+  {
+    "revision": "0ab42b7dcb8f7ef3b449ca8588e8c110",
+    "url": "/mount-and-blade-landing/manifest.json"
+  }
+]);
