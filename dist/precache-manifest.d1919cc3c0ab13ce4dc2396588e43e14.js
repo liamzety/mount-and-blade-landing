19d27b8ba8b80eac6302f906cbdc858e808ef907
liamzety <58632095+liamzety@users.noreply.github.com>
2020-12-10 14:03:46 +0200

    prod

---
+++ dist/precache-manifest.d1919cc3c0ab13ce4dc2396588e43e14.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "492fb9ede93df993d373",
+    "url": "/mount-and-blade-landing/css/app.3277ceb4.css"
+  },
+  {
+    "revision": "47a69df010ec81bd9bb2afa95605dbe1",
+    "url": "/mount-and-blade-landing/fonts/Merienda-Regular.47a69df0.ttf"
+  },
+  {
+    "revision": "271afd042ffe7fb79274e7a13e6c6dba",
+    "url": "/mount-and-blade-landing/fonts/Texturina-Regular-opsz=36.271afd04.ttf"
+  },
+  {
+    "revision": "677d69dba82f741e9bb9a1a5b8376e43",
+    "url": "/mount-and-blade-landing/img/1.677d69db.png"
+  },
+  {
+    "revision": "ae75bfb68cbe6234ac6fd81697b747a0",
+    "url": "/mount-and-blade-landing/img/2.ae75bfb6.png"
+  },
+  {
+    "revision": "22224ca28491d64b67512275a7f1057d",
+    "url": "/mount-and-blade-landing/img/3.22224ca2.png"
+  },
+  {
+    "revision": "91cd2f35df7d0b0bcc409017a23dc808",
+    "url": "/mount-and-blade-landing/img/4.91cd2f35.png"
+  },
+  {
+    "revision": "0eaf4494e1605367ba7eaa2a4f7f7f5e",
+    "url": "/mount-and-blade-landing/img/5.0eaf4494.png"
+  },
+  {
+    "revision": "7ca43d5b4c0d2ceed18c9035f460f329",
+    "url": "/mount-and-blade-landing/img/6.7ca43d5b.png"
+  },
+  {
+    "revision": "359c0fc91cc88dfd3b73c148f2aa2f7b",
+    "url": "/mount-and-blade-landing/img/battlefield-bg.359c0fc9.jpg"
+  },
+  {
+    "revision": "b36d6408065c0b7b4ea724455e9ba1e2",
+    "url": "/mount-and-blade-landing/img/bg-tw.b36d6408.jpg"
+  },
+  {
+    "revision": "7343bc24d8c02fdd865b389be0129371",
+    "url": "/mount-and-blade-landing/img/black_smug.7343bc24.png"
+  },
+  {
+    "revision": "50cc8977f8f4af66a9e410d817512e6f",
+    "url": "/mount-and-blade-landing/img/logo.50cc8977.png"
+  },
+  {
+    "revision": "8c0d216e726dc28716b5a5aba84ed03f",
+    "url": "/mount-and-blade-landing/img/mnb-bannerlord.8c0d216e.jpg"
+  },
+  {
+    "revision": "af6f247e63347d8cde9ee23610fe8890",
+    "url": "/mount-and-blade-landing/img/mnb-napoleonic-wars.af6f247e.jpg"
+  },
+  {
+    "revision": "f6dbd470a42b55305cfdb4005e05e1fa",
+    "url": "/mount-and-blade-landing/img/mnb-viking-conquest.f6dbd470.jpg"
+  },
+  {
+    "revision": "34298dbc6f748cbf346dfbf85075b5ab",
+    "url": "/mount-and-blade-landing/img/mnb-warband.34298dbc.jpg"
+  },
+  {
+    "revision": "5babcecf2419ba35b07daf3a6f5d7e49",
+    "url": "/mount-and-blade-landing/img/mnb-with-fire-and-sword.5babcecf.jpg"
+  },
+  {
+    "revision": "b7452c9acbd3eeb428c1887aea6be4f4",
+    "url": "/mount-and-blade-landing/img/mnb.b7452c9a.jpg"
+  },
+  {
+    "revision": "f8852682afb40ded4fc73595cf6da3bf",
+    "url": "/mount-and-blade-landing/img/rider.f8852682.png"
+  },
+  {
+    "revision": "8db409d600fe70947a7b013f2ca23bed",
+    "url": "/mount-and-blade-landing/index.html"
+  },
+  {
+    "revision": "492fb9ede93df993d373",
+    "url": "/mount-and-blade-landing/js/app.4faaf506.js"
+  },
+  {
+    "revision": "e0665e18e2505334a469",
+    "url": "/mount-and-blade-landing/js/chunk-vendors.8f37ac20.js"
+  },
+  {
+    "revision": "0ab42b7dcb8f7ef3b449ca8588e8c110",
+    "url": "/mount-and-blade-landing/manifest.json"
+  }
+]);
